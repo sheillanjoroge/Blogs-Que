596400e046cbf6623e03f9538506e2a1c2bbba11
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-12-14 17:09:22 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:zayheim01@localhost/blogs'
     
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/images'
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
